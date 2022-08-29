9138c9ec1aec56b7e9098924ee4bfb67a2e8ed9c
sunboywu <2845112989@qq.com>
2022-08-29 15:30:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- 头部区域 -->
+    <Header></Header>
+
+    <!-- 路由占位符 -->
+    <router-view></router-view>
+
+    <!-- 底部区域 -->
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header/index.vue'
+import Footer from './components/Footer/index.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'MyApp',
+  components: { Header, Footer },
+  mounted() {
+    // 通知Vuex发请求，获取数据，存储于仓库中
+    this.$store.dispatch('categoryList')
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less" scoped></style>
